9958d0ace31e68efe0183a32a83227cf76610df6
GustavoSFer <gustavo.sfernander@gmail.com>
2026-01-12 21:20:43 -0300

    feat: Iniciando o projeto

---
+++ src/main/java/br/com/fernandes/scheduler_backend_java/SchedulerBackendJavaApplication.java
@@ -0,0 +1,13 @@
+package br.com.fernandes.scheduler_backend_java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchedulerBackendJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchedulerBackendJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/fernandes/scheduler_backend_java/SchedulerBackendJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fernandes.scheduler_backend_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchedulerBackendJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
